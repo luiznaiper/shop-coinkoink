09b874fb8cabe65d8f7fd8df39236c3dbccf7eb8
luiznaiper <luiz_naiper@hotmail.com>
2022-06-18 14:20:56 -0500

    :construction: Added routes, webpack not working

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export { Home } from './Home';
+export { Checkout } from './CheckOut';
+export { Information } from './Information';
+export { Payment } from './Payment';
+export { Success } from './Success';
+export { NotFound } from './NotFound';
