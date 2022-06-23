a3a193811bb6f125451656508426267c4779278d
luiznaiper <luiz_naiper@hotmail.com>
2022-06-23 18:20:42 -0500

    :construction: Add geolocation api

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,18 +1,21 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { Map } from '../components/Map';
+import { useGoogleAddress } from '../hooks/useGoogleAddress';
 import '../styles/components/Success.css';
 
 const Success = () => {
   const { state } = useContext(AppContext);
   const { buyer } = state;
+  const location = useGoogleAddress(buyer[0].address);
+
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
+        <h2>{`${buyer[0].name}, Gracias por tu compra`}</h2>
         <span>Tu pedido va a llegar en 5 días a tu dirección</span>
         <div className="Success map">
-          <Map />
+          <Map data={location} />
         </div>
       </div>
     </div>
