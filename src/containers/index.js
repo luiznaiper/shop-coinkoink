c837beea2266e2986dfa59c6299962740bc798e5
luiznaiper <luiz_naiper@hotmail.com>
2022-06-29 13:21:14 -0500

    :see_no_evil: Untrack dist

--- src/containers/index.js
+++
@@ -1,6 +0,0 @@
-export { Home } from './Home';
-export { Checkout } from './Checkout';
-export { Information } from './Information';
-export { Payment } from './Payment';
-export { Success } from './Success';
-export { NotFound } from './NotFound';
